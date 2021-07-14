5fca3fcc49964728bc2c453763859f4d8e631ce5
li.chen <li.chen@sainsburys.co.uk>
2021-07-14 14:24:42 +0100

    reformatted files

--- src/main/java/AlgoTrading/apis/AlphaVantageApi.java
+++ src/main/java/AlgoTrading/apis/AlphaVantageApi.java
@@ -8,41 +8,70 @@ import java.net.http.HttpResponse;
 
 public class AlphaVantageApi {
 
-    public String getDataByFunction(String interval, String function, String symbol, String compactOrNot) throws IOException, InterruptedException {
-        interval = interval.isEmpty()? "" : "interval=" + interval;
-        function = "function=" + function.toUpperCase().replace(' ', '_');
-        symbol = "symbol=" + symbol.toUpperCase();
-        compactOrNot = "output_size=" + (compactOrNot.equalsIgnoreCase("compact")? "compact": "full");
+  public String getDataByFunction(
+      String interval, String function, String symbol, String compactOrNot)
+      throws IOException, InterruptedException {
+    interval = interval.isEmpty() ? "" : "interval=" + interval;
+    function = "function=" + function.toUpperCase().replace(' ', '_');
+    symbol = "symbol=" + symbol.toUpperCase();
+    compactOrNot = "output_size=" + (compactOrNot.equalsIgnoreCase("compact") ? "compact" : "full");
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?" + interval + '&' + function + '&' + symbol + "&datatype=csv&" + compactOrNot))
-                .header("x-rapidapi-key", "f5da0dee66msh4092453d2c85d7ap16c5c0jsn42ed0a42276c")
-                .header("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(" Request URI is " + request.uri());
-        System.out.println(response.body());
+    HttpRequest request =
+        HttpRequest.newBuilder()
+            .uri(
+                URI.create(
+                    "https://alpha-vantage.p.rapidapi.com/query?"
+                        + interval
+                        + '&'
+                        + function
+                        + '&'
+                        + symbol
+                        + "&datatype=csv&"
+                        + compactOrNot))
+            .header("x-rapidapi-key", "f5da0dee66msh4092453d2c85d7ap16c5c0jsn42ed0a42276c")
+            .header("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
+            .method("GET", HttpRequest.BodyPublishers.noBody())
+            .build();
+    HttpResponse<String> response =
+        HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    System.out.println("Request URI is " + request.uri());
+    System.out.println(response.body());
 
-        return response.body();
-    }
+    return response.body();
+  }
 
-    public String getDataByTechnicalIndicator(String interval, String seriesType, String function, String symbol, String timePeriod) throws IOException, InterruptedException {
-        interval = interval.isEmpty()? "" : "interval=" + interval;
-        seriesType = "series_type=" + seriesType;
-        function = "function=" + function.toUpperCase().replace(' ', '_');
-        symbol = "symbol=" + symbol.toUpperCase();
-        timePeriod = "time_period=" + timePeriod;
+  public String getDataByTechnicalIndicator(
+      String interval, String seriesType, String function, String symbol, String timePeriod)
+      throws IOException, InterruptedException {
+    interval = interval.isEmpty() ? "" : "interval=" + interval;
+    seriesType = "series_type=" + seriesType;
+    function = "function=" + function.toUpperCase().replace(' ', '_');
+    symbol = "symbol=" + symbol.toUpperCase();
+    timePeriod = "time_period=" + timePeriod;
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?" + interval + "&" + seriesType + "&" + function + "&" + symbol + "&" + timePeriod + "&datatype=csv"))
-                .header("x-rapidapi-key", "f5da0dee66msh4092453d2c85d7ap16c5c0jsn42ed0a42276c")
-                .header("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+    HttpRequest request =
+        HttpRequest.newBuilder()
+            .uri(
+                URI.create(
+                    "https://alpha-vantage.p.rapidapi.com/query?"
+                        + interval
+                        + "&"
+                        + seriesType
+                        + "&"
+                        + function
+                        + "&"
+                        + symbol
+                        + "&"
+                        + timePeriod
+                        + "&datatype=csv"))
+            .header("x-rapidapi-key", "f5da0dee66msh4092453d2c85d7ap16c5c0jsn42ed0a42276c")
+            .header("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
+            .method("GET", HttpRequest.BodyPublishers.noBody())
+            .build();
+    HttpResponse<String> response =
+        HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    System.out.println(response.body());
 
-        return response.body();
-    }
+    return response.body();
+  }
 }
--- src/main/java/AlgoTrading/parsers/SMAParser.java
+++ src/main/java/AlgoTrading/parsers/SMAParser.java
@@ -1,6 +1,5 @@
 package AlgoTrading.parsers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,10 +8,12 @@ public class SMAParser {
 
   public List<SMA> parseStringToSMAList(String input) {
     return Arrays.stream(input.replaceAll("[a-zA-Z]", "").substring(1).trim().split("\n"))
-        .map(line -> SMA.builder()
-                .date(line.substring(0, 16))
-                .sma(line.substring(17, line.length()-1))
-                .build())
+        .map(
+            line ->
+                SMA.builder()
+                    .date(line.substring(0, 16))
+                    .sma(line.substring(17, line.length() - 1))
+                    .build())
         .collect(Collectors.toList());
   }
 }
