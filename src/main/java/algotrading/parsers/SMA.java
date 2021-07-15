7a90bc86cb94bd63da55a475aec150eb7ade6457
li.chen <li.chen@sainsburys.co.uk>
2021-07-15 13:51:19 +0100

    tidied up imports

--- src/main/java/algotrading/parsers/SMA.java
+++ src/main/java/algotrading/parsers/SMA.java
@@ -1,11 +1,14 @@
-package algotrading.parsers;
+package AlgoTrading.parsers;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Date;
 
 @Builder
 @AllArgsConstructor
 public class SMA {
-  String date;
-  String sma;
+    String date;
+    String sma;
 }
