b6ab8b28c7792a595264be77aa3a514165b25bc2
l-y-c <li.chen@sainsburys.co.uk>
2021-07-20 14:48:40 +0100

    created STOCHParser and implemented parsing method

---
+++ src/main/java/algotrading/parsers/STOCHParser.java
@@ -0,0 +1,21 @@
+package algotrading.parsers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class STOCHParser {
+
+  public List<STOCH> parseStringToStochList(String input) {
+    return Arrays.stream(input.replaceAll("[a-zA-Z]", "").substring(5).trim().split("\n"))
+        .map(
+            line ->
+                STOCH
+                    .builder()
+                    .time(line.split(",")[0])
+                    .slowD(line.split(",")[1])
+                    .slowK(line.split(",")[2].trim())
+                    .build())
+        .collect(Collectors.toList());
+  }
+}
