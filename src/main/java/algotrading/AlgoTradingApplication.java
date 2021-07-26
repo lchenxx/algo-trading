bd7775c565ea08abfdea8236d7acc7da9b71d36b
l-y-c <li.chen@sainsburys.co.uk>
2021-07-26 10:57:11 +0100

    added methods to retrieve AV data

--- src/main/java/algotrading/AlgoTradingApplication.java
+++ src/main/java/algotrading/AlgoTradingApplication.java
@@ -1,18 +1,31 @@
 package algotrading;
 
 import algotrading.common.AlphaVantageApi;
-import algotrading.parsers.SMAParser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class AlgoTradingApplication {
   public static void main(String[] args) throws IOException, InterruptedException {
     AlphaVantageApi api = new AlphaVantageApi();
-    //        api.getDataByFunction("TIME_SERIES_INTRADAY", "AMZN", "5min", "compact");
-    //        api.getRSIData("AMZN", "weekly", "10", "close");
-
-    SMAParser parser = new SMAParser();
-    parser.parseStringToSMAList(
-        api.getDataByFunction("TIME_SERIES_INTRADAY", "AMZN", "5min", "compact"));
+    api.getRawDataByTickers("TIME_SERIES_DAILY", "5min", "compact");
+    api.getTechnicalDataByTickers("RSI", "weekly", "10");
+    //    api.getDataByFunction("TIME_SERIES_INTRADAY", "AMZN", "5min", "compact");
+    //
+    //    RSIParser rsiParser = new RSIParser();
+    //    rsiParser.parseStringToRSIList(api.getDataByIndicator("RSI", "AMZN", "weekly", "10"));
+    //
+    //    STOCHParser stochParser = new STOCHParser();
+    //    stochParser.parseStringToStochList(api.getDataByIndicator("STOCH", "AMZN", "weekly",
+    // null));
+    //
+    //    MACDParser macdParser = new MACDParser();
+    //    macdParser.parseStringToMacdList(api.getDataByIndicator("MACD", "AMZN", "weekly", null));
   }
+
+  //  public static void main(String[] args){
+  //    SpringApplication.run(AlgoTradingApplication.class, args);
+  //  }
 }
