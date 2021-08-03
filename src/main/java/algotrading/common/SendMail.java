5540b7dbe438bd3fe91328a4f7692b8756b92970
l-y-c <li.chen@sainsburys.co.uk>
2021-08-03 19:44:55 +0100

    added functionality to send email using gmail smtp

---
+++ src/main/java/algotrading/common/SendMail.java
@@ -0,0 +1,56 @@
+package algotrading.common;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+
+  public void sendEmail(String input) {
+
+    String to = "<receiver email>";
+    String from = "<sender email>";
+    String host = "smtp.gmail.com";
+
+    Properties properties = System.getProperties();
+    properties.put("mail.smtp.host", host);
+    properties.put("mail.smtp.port", "465");
+    properties.put("mail.smtp.ssl.enable", "true");
+    properties.put("mail.smtp.auth", "true");
+
+    Session session =
+        Session.getInstance(
+            properties,
+            new javax.mail.Authenticator() {
+
+              protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(
+                    System.getenv("EMAIL"), System.getenv("PASSWORD"));
+              }
+            });
+
+    // Used to debug SMTP issues
+    session.setDebug(true);
+
+    try {
+      MimeMessage message = new MimeMessage(session);
+      message.setFrom(new InternetAddress(from));
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+      message.setSubject("Ratings for tickers of interest " + new Timestamp(new Date().getTime()));
+      message.setText(input);
+
+      Transport.send(message);
+      System.out.println("Message sent successfully....");
+    } catch (MessagingException mex) {
+      mex.printStackTrace();
+    }
+  }
+}
